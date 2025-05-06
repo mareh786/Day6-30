71a7d83ada891018b9732bfaa23fbe86e4aebce3
Ubuntu <ubuntu@ip-172-31-14-222.ap-south-1.compute.internal>
2025-05-06 12:39:29 +0000

    Added All Files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/greet', (req, res) => {
+  const name = req.body.name;
+  res.render('greet', { name });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
